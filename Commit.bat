3a0cb0be16e1b64ea90ad94a58af78efdb8a2b5f
Talox <arne@vanderlei.be>
2023-02-13 13:34:49 +0100

    main optimization push

---
+++ Commit.bat
@@ -0,0 +1,3 @@
+git -C F:\The-Enigma-Universe add *
+git -C F:\The-Enigma-Universe commit text\Data.json
+git -C F:\The-Enigma-Universe push
--- ProjectImageLoading/Assets/Scripts/World.cs
+++ ProjectImageLoading/Assets/Scripts/World.cs
@@ -3,6 +3,7 @@ using UdonSharp;
 using UnityEngine;
 using UnityEngine.Rendering;
 using VRC.SDK3.Image;
+using VRC.SDK3.StringLoading;
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common.Enums;
@@ -10,71 +11,23 @@ using VRC.Udon.Common.Interfaces;
 
 public class World : UdonSharpBehaviour
 {
-
-    private VRCImageDownloader Downloader;
     public UdonBehaviour behaviour;
     public VRCUrl URL;
 
-    public MeshRenderer Renderer;
-    public MeshFilter filter;
-    public MeshCollider collider;
-
-    private Vector3[][] HeightLookup = new Vector3[][] 
-    {
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 1)},
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1)},
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 1, 1)},
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(0, 0, 1), new Vector3(1, 1, 1)},
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 1)},
-        new Vector3[]{ new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 1)},
-        new Vector3[]{ new Vector3(0, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 1)},
-        new Vector3[]{ new Vector3(0, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 1), new Vector3(1, 0, 1)},
-        new Vector3[]{ new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 1), new Vector3(1, 0, 1)},
-    };
-
-    private Vector2[][] UVLookup = new Vector2[][]
-    {
-        new Vector2[]{ new Vector2(0.4f, 0.6f), new Vector2(0.6f, 0.6f), new Vector2(0.4f, 0.4f), new Vector2(0.6f, 0.4f) },
-    };
-
-    private int[][] TriLookup = new int[][]
-    {
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 2, 1, 1, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 2, 1, 1, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3},
-        new int[]{0, 3, 1, 0, 2, 3}
-    };
-
-
     public GameObject[] Prefabs;
-    public Texture2D DownloadedTexture;
+    public GameObject[] EntityModels;
 
     public Transform PrefabList;
-    /// <summary>
-    /// Section
-    /// </summary>
-    private Color32[] pixels;
-    private Vector3[] vertices;
-    private Vector2[] uvs;
-    private int[] tris;
-    private int indexVert = 0;
-    private int indextri = 0;
-    private int indexUV = 0;
-    private int width;
-    private int height;
-    private Vector3 offset;
-    private int y;
+    public Transform PrefabModelList;
+    public Chunk chunkPrefab;
+    public Entity EntityPrefab;
+
+    private object[] world;
+
 
     void Start()
     {
-        Downloader = new VRCImageDownloader();
-
-        Downloader.DownloadImage(URL, null, behaviour);
+        VRCStringDownloader.LoadUrl(URL, behaviour);
 
         Prefabs = new GameObject[PrefabList.childCount];
 
@@ -84,114 +37,33 @@ public class World : UdonSharpBehaviour
         }
 
         PrefabList.gameObject.SetActive(false);
+        PrefabModelList.gameObject.SetActive(false);
     }
 
-    public override void OnImageLoadSuccess(VRC.SDK3.Image.IVRCImageDownload result)
+    public override void OnStringLoadSuccess(IVRCStringDownload result)
     {
-        DownloadedTexture = result.Result;
-        pixels = DownloadedTexture.GetPixels32();
+        world = JsonParser.ImportJson(result.Result).GetJsonObject("levels");
+
         Debug.Log("Success");
-        GenerateTerrain();
-    }
-
-    public override void OnImageLoadError(IVRCImageDownload result)
-    {
-        Debug.Log("Failiure: " + result.Error + " , " + result.ErrorMessage);
-    }
-
-    public void GenerateTerrain()
-    {
-        width = DownloadedTexture.width;
-        height = DownloadedTexture.height;
-        offset = new Vector3(width, 0, height) / 2;
-
-        int size = 4 * width * height; 
-
-        vertices = new Vector3[size];
-        uvs = new Vector2[size];
-        tris = new int[6 * width * height];
-
-        indexVert = 0;
-        indextri = 0;
-        indexUV = 0;
-
-        y = 0;
-
-        SendCustomEventDelayedFrames("ProcessWidthDeffered", 1, EventTiming.Update);
-    }
-
-    public void ProcessWidthDeffered()
-    {
-        for (int x = 0; x < width; x++)
+        int count = world.GetJsonCount();
+        for (int i = 0; i < count; i++)
         {
-            int index = x + y * width;
-            int green = pixels[index].g;
-            int value = (green & 0b00001111);
-            int color = green >> 4;
-            int heightCel = pixels[index].r;
-            int indexPrefabs = pixels[index].b - 1;
-            Vector2 colorOffset = new Vector2(color & 0b0011, (color & 0b1100) >> 2) / 4;
-
-            Vector3[] vertToAdd = HeightLookup[value];
-            Vector2[] UVToAdd = UVLookup[0];
-            int[] TriToAdd = TriLookup[value];
-
-            if( indexPrefabs != -1)
-            {
-                GameObject gameObject = Prefabs[indexPrefabs];
-
-                GameObject instansiated = Object.Instantiate(gameObject, new Vector3(x, heightCel, y) - offset + new Vector3(0.5f,0,0.5f), gameObject.transform.rotation);
-
-                instansiated.transform.SetParent(transform);
-            }
-
-            for (int j = 0; j < TriToAdd.Length; j++)
-            {
-                tris[indextri] = indexVert + TriToAdd[j];
-                indextri++;
-            }
-
-            for (int j = 0; j < vertToAdd.Length; j++)
-            {
-                vertices[indexVert] = vertToAdd[j] + new Vector3(x, heightCel, y) - offset;
-                indexVert++;
-            }
-
-            for (int j = 0; j < UVToAdd.Length; j++)
-            {
-                uvs[indexUV] = (UVToAdd[j] / 4f) + colorOffset;
-                indexUV++;
-            }
-        }
-
-        y++;
-
-        if(y < height)
-        {
-            SendCustomEventDelayedFrames("ProcessWidthDeffered", 1);
-        }
-        else
-        {
-            Mesh mesh = filter.sharedMesh;
-
-            if (mesh == null)
-            {
-                mesh = new Mesh();
-                mesh.indexFormat = IndexFormat.UInt32;
-            }
-
-            mesh.vertices = vertices;
-            mesh.triangles = tris;
-            mesh.uv = uvs;
-            mesh.RecalculateNormals();
-
-            filter.sharedMesh = mesh;
-            collider.sharedMesh = mesh;
+            GenerateTerrain(world.GetJsonKey(i));
         }
     }
 
-    public void OnDestroy()
+    public override void OnStringLoadError(IVRCStringDownload result)
     {
-        Downloader.Dispose();
+        Debug.Log("Failure: " + result.Error + " , " + result.ErrorCode);
     }
+
+    public void GenerateTerrain(string index)
+    {
+        GameObject gameObject = Instantiate(chunkPrefab.gameObject);
+        Chunk chunk = gameObject.GetComponent<Chunk>();
+        chunk.json = world.GetJsonObject(index);
+        chunk.GenerateChunk();
+        chunk.transform.SetParent(transform);
+    }
+
 }
